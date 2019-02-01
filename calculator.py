6dc6e605b35bda139eb281be7fa77dc116d6a901
Chris.Zhou <jz200@duke.edu>
2019-02-01 14:21:30 -0500

    Took out cov from requirements

--- calculator.py
+++ calculator.py
@@ -1,3 +1,14 @@
 def add_two_numbers(a,b):
     c = a + b
     return c
+
+
+def subtract_two_numbers(a,b):
+    return a-b
+
+def find_min(a):
+    return min(a)
+
+
+def find_max(a):
+    return max(a)
--- test_calculator.py
+++ test_calculator.py
@@ -8,4 +8,29 @@ import pytest
 def test_calculator(a,b,expected):
     from calculator import add_two_numbers
     answer = add_two_numbers(a,b)
-    assert answer == expected
+    assert answer == pytest.approx(expected)
+
+@pytest.mark.parametrize('a,b,expected',[
+(3,2,1),
+(5,4,1)
+])
+def test_subtract(a,b,expected):
+    from calculator import subtract_two_numbers
+    assert subtract_two_numbers(a,b) == expected
+
+
+@pytest.mark.parametrize('a,expected',[
+([3,4,5],5),
+([6,7,8],8)
+])
+def test_findmax(a,expected):
+    from calculator import find_max
+    assert find_max(a) == expected
+
+@pytest.mark.parametrize('a,expected',[
+([3,4,5],3),
+([6,7,8],6)
+])
+def test_findmin(a,expected):
+    from calculator import find_min
+    assert find_min(a) == expected
