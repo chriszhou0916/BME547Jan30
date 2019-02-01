4c182eb0d88bd2ebf8279ec096c6a055f65991eb
Chris.Zhou <jz200@duke.edu>
2019-02-01 14:08:33 -0500

    added calculator

--- test_calculator.py
+++ test_calculator.py
@@ -1,10 +1,11 @@
-@pytest.mark.parameterize('a,b,expected',[
+import pytest
+@pytest.mark.parametrize('a,b,expected',[
 (2,3,5),
-(5.-2.3),
+(5,-2,3),
 (10,20,30)
 ])
 # this is a decorator. runs this test multiple times with different inputs
-def test_calculator():
+def test_calculator(a,b,expected):
     from calculator import add_two_numbers
-    answer = add_two_numbers(2,3)
-    assert answer == 5
+    answer = add_two_numbers(a,b)
+    assert answer == expected
